4d8fd6c3aa79d1127088e3fc5a5e218b09303999
Gabriel Ribeiro <gabriel.ribeiro@quintoandar.com.br>
2019-04-20 22:15:28 -0300

    fix var

--- ACH2024-2019-1SEM/ep1/prim.c
+++ ACH2024-2019-1SEM/ep1/prim.c
@@ -5,10 +5,11 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
+// GRAPH
 typedef struct AdjacencyListNode { 
 	int vertex; 
-	int weight; 
-	struct AdjListNode* next; 
+	int weight;
+    struct AdjacencyListNode *next;
 } AdjacencyListNode;
 
 typedef struct AdjacencyList { 
@@ -20,48 +21,19 @@ typedef struct Graph {
     struct AdjacencyList* adjList; 
 } Graph;
 
-AdjacencyListNode *newAdjacencyListNode(int destination, int weight) {
-    AdjacencyListNode *newNode = malloc(sizeof(AdjacencyListNode));
-    newNode->vertex = destination;
-    newNode->weight = weight;
-    newNode->next = NULL;
-    return newNode;
-}
-Graph* createGraph(int size) {
-    Graph* graph = malloc(sizeof(Graph));
-    graph->size = size;
+AdjacencyListNode *newAdjacencyListNode(int destination, int weight);
+Graph *createGraph(int size);
+void addEdge(Graph *graph, int source, int destination, int weight);
+void addNodeInAdjacencyList(Graph *graph, int adjListVertex, int vertex, int weight);
+void printGraph(Graph *graph);
 
-    graph->adjList = malloc(size * sizeof(AdjacencyList));
-    for(int i=0; i<size; i++) {
-        graph->adjList[i].head = NULL;
-    }
-    return graph;
-}
+//HEAP
+typedef struct Heap {
+    int *arr;
+    int count;
+    int capacity;
+} Heap;
 
-// non-directed graph
-void addEdge(Graph* graph, int source, int destination, int weight) {
-    addNodeInAdjacencyList(&graph, source, destination, weight);
-    addNodeInAdjacencyList(&graph, destination, source, weight);
-}
-
-void addNodeInAdjacencyList(Graph *graph, int adjListVertex, int vertex, int weight) {
-    AdjacencyListNode *newNode = newAdjacencyListNode(vertex, weight);
-    newNode->next = graph->adjList[adjListVertex].head;
-    graph->adjList[adjListVertex].head = newNode;
-}
-
-void printGraph(Graph* graph, int size) {
-    for(int i = 0; i<size; i++) {
-        printf("Adjacency list of %d |", i);
-        AdjacencyListNode* node = graph->adjList[i].head;
-
-        while (node != NULL) {
-            printf(" %d", node->vertex);
-            node = node->next;
-        }
-        printf("\n");
-    }
-}
 
 void main() {
     int sizeGraph = 9;
@@ -70,20 +42,78 @@ void main() {
     Graph* graph = createGraph(sizeGraph);
 
     addEdge(graph, 0, 1, 4);
-    // addEdge(graph, 0, 7, 8);
-    // addEdge(graph, 1, 2, 8);
-    // addEdge(graph, 1, 7, 11);
-    // addEdge(graph, 2, 3, 7);
-    // addEdge(graph, 2, 8, 2);
-    // addEdge(graph, 2, 5, 4);
-    // addEdge(graph, 3, 4, 9);
-    // addEdge(graph, 3, 5, 14);
-    // addEdge(graph, 4, 5, 10);
-    // addEdge(graph, 5, 6, 2);
-    // addEdge(graph, 6, 7, 1);
-    // addEdge(graph, 6, 8, 6);
-    // addEdge(graph, 7, 8, 7);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
+    printGraph(graph);
+
     // for(int i =0; i < totalEdges; i++ ) {
     //     addEdge(1, 2, 3);
     // }
+}
+
+AdjacencyListNode* newAdjacencyListNode(int destination, int weight) {
+    AdjacencyListNode *newNode = malloc(sizeof(AdjacencyListNode));
+    newNode->vertex = destination;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+Graph* createGraph(int size) {
+    Graph *graph = malloc(sizeof(Graph));
+    graph->size = size;
+
+    graph->adjList = malloc(size * sizeof(AdjacencyList));
+    for (int i = 0; i < size; i++)
+    {
+        graph->adjList[i].head = NULL;
+    }
+    return graph;
+}
+
+// non-directed graph
+void addEdge(Graph *graph, int source, int destination, int weight) {
+    addNodeInAdjacencyList(graph, source, destination, weight);
+    addNodeInAdjacencyList(graph, destination, source, weight);
+}
+
+void addNodeInAdjacencyList(Graph *graph, int adjListVertex, int vertex, int weight) {
+    AdjacencyListNode *newNode = newAdjacencyListNode(vertex, weight);
+    newNode->next = graph->adjList[adjListVertex].head;
+    graph->adjList[adjListVertex].head = newNode;
+}
+
+void printGraph(Graph *graph) {
+    int size = graph->size;
+    for (int i = 0; i < size; i++)
+    {
+        printf("Adjacency list of %d |", i);
+        AdjacencyListNode *node = graph->adjList[i].head;
+
+        while (node != NULL)
+        {
+            printf(" %d", node->vertex);
+            node = node->next;
+        }
+        printf("\n");
+    }
+}
+
+Heap *CreateHeap(int capacity) {
+    Heap *heap = (Heap *)malloc(sizeof(Heap));
+    heap->count = 0;
+    heap->capacity = capacity;
+    heap->arr = (int *)malloc(capacity * sizeof(int));
+
+    return heap;
 }
