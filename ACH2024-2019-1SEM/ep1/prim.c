c9e06411ad272cfbd4133c8452e2a09e409702bc
Gabriel Ribeiro <gabriel.ribeiro@quintoandar.com.br>
2019-04-30 21:46:46 -0300

    added comments e code formatter

--- ACH2024-2019-1SEM/ep1/prim.c
+++ ACH2024-2019-1SEM/ep1/prim.c
@@ -7,7 +7,7 @@
 #include <limits.h>
 #include <stdbool.h>
 
-// Graph structure
+// Estruturas para Grafo
 typedef struct AdjacencyListNode
 {
     int vertex;
@@ -23,13 +23,13 @@ typedef struct AdjacencyList
 typedef struct Graph
 {
     int size;
-    int* visited;
-    int* ant;
-    int* cost;
+    int *visited;
+    int *ant;
+    int *cost;
     struct AdjacencyList *adjList;
 } Graph;
 
-// Heap structure
+// Estruturas para Heap
 typedef struct HeapNode
 {
     int value;
@@ -44,78 +44,91 @@ typedef struct Heap
     HeapNode **array;
 } Heap;
 
-// Graph Methods
+// Metodos para manipulação de Grafo
 AdjacencyListNode *newAdjacencyListNode(int destination, int weight);
 Graph *createGraph(int size);
 void addEdge(Graph *graph, int source, int destination, int weight);
 void addNodeInAdjacencyList(Graph *graph, int adjListVertex, int vertex, int weight);
 Graph *createAndPopulateGraphFromFile(char *filenameIn);
 
-// Heap methods
+// Metodos para manipulação de Heap
 HeapNode *newHeapNode(int value, int key);
 Heap *createMinHeap(int capacity);
 void swapHeapNode(HeapNode **a, HeapNode **b);
 void minHeapify(Heap *minHeap, int idx);
 int isMinHeapEmpty(Heap *minHeap);
 bool insideOfMinHeap(Heap *minHeap, int value);
-void decreaseKey(Heap *minHeap, int value, int key);
+void decreaseCost(Heap *minHeap, int value, int key);
 HeapNode *extractMin(Heap *minHeap);
 
-//file method
+//Metodos para manipulação de arquivos
 bool isFileExists(FILE *file, char *filename);
 void writeMSTTotalCost(int cost, char *filenameOut);
 void writeMSTEdges(int vertex_1, int vertex_2, char *filenameOut);
 
-// main method
+// Metodos principais do EP
 void printMSTwithDFS(Graph *graph, int vertex, char *fileName);
 void printEdgesMST(Graph *graph, char *filenameOut);
-Graph* prim(Graph *graph);
+Graph *prim(Graph *graph);
 
-void main(int argc, char *argv[]){
+// METODO MAIN
+void main(int argc, char *argv[])
+{
     char *filenameIn = argv[1];
     char *filenameOut = argv[2];
 
-    Graph* graph = createAndPopulateGraphFromFile(filenameIn);
-    
+    //cria e popula grafo
+    Graph *graph = createAndPopulateGraphFromFile(filenameIn);
 
+    //roda o algoritimo prim
     graph = prim(graph);
+
+    //escreve no arquivo de saida a AGM e seu custo
     printEdgesMST(graph, filenameOut);
+
+    exit(0); // sucesso
 }
 
-// main method
-Graph* prim(Graph *graph)
+// METODOS PRINCIPAIS
+
+// Aplica o algoritimo Prim usando MinHeap para extrair o vertice de menor custo
+Graph *prim(Graph *graph)
 {
     int size = graph->size;
-    int *parent = graph->ant;
+    int *ant = graph->ant;
     int *cost = graph->cost;
 
     Heap *minHeap = createMinHeap(size);
 
-    for (int i = 1; i < size; ++i)
+    for (int i = 1; i < size; ++i) // inicializa variaveis
     {
         minHeap->array[i] = newHeapNode(i, cost[i]);
         minHeap->position[i] = i;
     }
 
+    // começa a partir do vertice 0
     cost[0] = 0;
     minHeap->array[0] = newHeapNode(0, cost[0]);
     minHeap->position[0] = 0;
-    minHeap->size = size;
+    minHeap->size = size; // inicializa tamanho do heap
 
-    while (!isMinHeapEmpty(minHeap)) {
-        HeapNode *minHeapNode = extractMin(minHeap);
-        int vertex_U = minHeapNode->value; //vertex u
+    while (!isMinHeapEmpty(minHeap))
+    {                                                // roda enquanto existir vertice no heap
+        HeapNode *minHeapNode = extractMin(minHeap); // busca vertice com menor custo
+        int vertex_U = minHeapNode->value;           //vertice U
 
         AdjacencyListNode *adjVertex_U = graph->adjList[vertex_U].head;
+        // foreach vizinho W adjacentes de U
         while (adjVertex_U != NULL)
         {
-            int vertex_V = adjVertex_U->vertex;
+            int vertex_W = adjVertex_U->vertex; //vertice W
 
-            if (insideOfMinHeap(minHeap, vertex_V) && adjVertex_U->weight < cost[vertex_V])
+            // se custo[w]> p(u, w) entao
+            if (insideOfMinHeap(minHeap, vertex_W) && cost[vertex_W] > adjVertex_U->weight)
             {
-                cost[vertex_V] = adjVertex_U->weight;
-                parent[vertex_V] = vertex_U;
-                decreaseKey(minHeap, vertex_V, cost[vertex_V]);
+                cost[vertex_W] = adjVertex_U->weight;
+                ant[vertex_W] = vertex_U;
+                decreaseCost(minHeap, vertex_W, cost[vertex_W]);
             }
             adjVertex_U = adjVertex_U->next;
         }
@@ -123,39 +136,54 @@ Graph* prim(Graph *graph)
 
     return graph;
 }
-void printEdgesMST(Graph *graph, char* filenameOut) {
-    int* edges = graph->ant;
-    int* cost = graph->cost;
+
+// Escreve no arquivo o custo da AGM e a AGM utilizando busca em
+// profundidade para printar a partir do nó raiz
+void printEdgesMST(Graph *graph, char *filenameOut)
+{
+    int *edges = graph->ant;
+    int *cost = graph->cost;
     int size = graph->size;
+    
     int totalCost = 0;
-    for (int i = 1; i < size; ++i) {
+    for (int i = 1; i < size; ++i) // calcula custo da AGM
+    {
         totalCost = totalCost + cost[i];
     }
     writeMSTTotalCost(totalCost, filenameOut);
 
-    Graph* graphPrint = createGraph(size);
-    for(int i=1; i<size; i++) {
+    // Cria grafo da MST para fazer a busca em profundidade para printar
+    Graph *graphPrint = createGraph(size); 
+    for (int i = 1; i < size; i++)
+    {
         addEdge(graphPrint, i, edges[i], 0);
     }
+
+    // printar a partir do nó raiz da AGM
     printMSTwithDFS(graphPrint, 0, filenameOut);
 }
 
-void printMSTwithDFS(Graph *graph, int vertex, char* fileName) {
+// Busca em profundidade e escrevendo no arquivo de saida
+void printMSTwithDFS(Graph *graph, int vertex, char *fileName)
+{
     AdjacencyListNode *adjVertex_U = graph->adjList[vertex].head;
-    graph->visited[vertex] = 1;
+    graph->visited[vertex] = 1; //visitado
 
-    while (adjVertex_U != NULL) {
-        int vertex_V = adjVertex_U->vertex;
-        if (graph->visited[vertex_V] == 0)
+    // foreach vizinho W adjacentes de U
+    while (adjVertex_U != NULL)
+    {
+        int vertex_W = adjVertex_U->vertex;
+        if (graph->visited[vertex_W] == 0)
         {
-            writeMSTEdges(vertex,vertex_V, fileName);
-            printMSTwithDFS(graph, vertex_V, fileName);
+            writeMSTEdges(vertex, vertex_W, fileName); // escreve no arquivo
+            printMSTwithDFS(graph, vertex_W, fileName);
         }
         adjVertex_U = adjVertex_U->next;
     }
 }
 
-// Graph methods
+// Metodos para manipulação de grafos (grafo não direcionado)
+
 AdjacencyListNode *newAdjacencyListNode(int destination, int weight)
 {
     AdjacencyListNode *newNode = malloc(sizeof(AdjacencyListNode));
@@ -165,6 +193,7 @@ AdjacencyListNode *newAdjacencyListNode(int destination, int weight)
 
     return newNode;
 }
+
 Graph *createGraph(int size)
 {
     Graph *graph = malloc(sizeof(Graph));
@@ -185,7 +214,6 @@ Graph *createGraph(int size)
     return graph;
 }
 
-// non-directed graph
 void addEdge(Graph *graph, int source, int destination, int weight)
 {
     addNodeInAdjacencyList(graph, source, destination, weight);
@@ -199,27 +227,28 @@ void addNodeInAdjacencyList(Graph *graph, int adjListVertex, int vertex, int wei
     graph->adjList[adjListVertex].head = newNode;
 }
 
-Graph *createAndPopulateGraphFromFile(char* filenameIn)
+// Método para criar e adicionar arestas a partir dos dados do arquivo de entrada
+Graph *createAndPopulateGraphFromFile(char *filenameIn)
 {
-     FILE* file = fopen(filenameIn, "r");
-     isFileExists(file, filenameIn);
-     
-     int sizeGraph, totalEdges;
-     fscanf(file, "%d %d", &sizeGraph, &totalEdges);
-     Graph *graph = createGraph(sizeGraph);
+    FILE *file = fopen(filenameIn, "r");
+    isFileExists(file, filenameIn);
 
-     int vertex_1, vertex_2, weight;
-     for (int i = 0; i < totalEdges; i++)
-     {
-         fscanf(file, "%d %d %d", &vertex_1, &vertex_2, &weight);
-         addEdge(graph, vertex_1, vertex_2, weight);
+    int sizeGraph, totalEdges;
+    fscanf(file, "%d %d", &sizeGraph, &totalEdges);
+    Graph *graph = createGraph(sizeGraph); // cria grafo
+
+    int vertex_1, vertex_2, weight;
+    for (int i = 0; i < totalEdges; i++) // cria arestas
+    {
+        fscanf(file, "%d %d %d", &vertex_1, &vertex_2, &weight);
+        addEdge(graph, vertex_1, vertex_2, weight);
     }
-
     fclose(file);
+
     return graph;
 }
 
-// Heap methods
+// Heap methods (Heap minino)
 HeapNode *newHeapNode(int value, int key)
 {
     HeapNode *heapNode = malloc(sizeof(HeapNode));
@@ -239,87 +268,104 @@ Heap *createMinHeap(int capacity)
 
     return minHeap;
 }
-
-void swapHeapNode(HeapNode **a, HeapNode **b)
+// troca posiçoes de dois nos
+void swapHeapNode(HeapNode **node_A, HeapNode **node_B)
 {
-    HeapNode *t = *a;
-    *a = *b;
-    *b = t;
+    HeapNode *node_TEMP = *node_A;
+    *node_A = *node_B;
+    *node_B = node_TEMP;
 }
 
-void minHeapify(Heap *minHeap, int idx) {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
+// Atualiza as posiçoes do nós
+void minHeapify(Heap *minHeap, int index)
+{
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
 
     if (left < minHeap->size && minHeap->array[left]->key < minHeap->array[smallest]->key)
+    {
         smallest = left;
-
+    }
     if (right < minHeap->size && minHeap->array[right]->key < minHeap->array[smallest]->key)
+    {
         smallest = right;
-
-    if (smallest != idx)
+    }
+    if (smallest != index)
     {
         HeapNode *smallestNode = minHeap->array[smallest];
-        HeapNode *idxNode = minHeap->array[idx];
+        HeapNode *indexNode = minHeap->array[index];
+        // troca posiçoes
+        minHeap->position[smallestNode->value] = index;
+        minHeap->position[indexNode->value] = smallest;
 
-        minHeap->position[smallestNode->value] = idx;
-        minHeap->position[idxNode->value] = smallest;
-
-        swapHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
-
-        minHeapify(minHeap, smallest);
+        // troca posiçao dos nós
+        swapHeapNode(&minHeap->array[smallest], &minHeap->array[index]);
+        minHeapify(minHeap, smallest); // O(log n)
     }
 }
 
-HeapNode *extractMin(Heap *minHeap) {
+// Aplica o heapify, retorna o vertice com menor custo.
+HeapNode *extractMin(Heap *minHeap)
+{
     if (isMinHeapEmpty(minHeap))
+    {
         return NULL;
+    }
+    HeapNode *root = minHeap->array[0]; // no raiz
 
-    HeapNode *root = minHeap->array[0];
-
-    HeapNode *lastNode = minHeap->array[minHeap->size - 1];
+    HeapNode *lastNode = minHeap->array[minHeap->size-1];
     minHeap->array[0] = lastNode;
 
-    minHeap->position[root->value] = minHeap->size - 1;
+    minHeap->position[root->value] = minHeap->size-1;
     minHeap->position[lastNode->value] = 0;
 
-    minHeap->size = minHeap->size -1;
+    minHeap->size = minHeap->size - 1; // a cada chamada, diminui o range de ordenação do heap
     minHeapify(minHeap, 0);
 
     return root;
 }
 
-int isMinHeapEmpty(Heap *minHeap) {
+int isMinHeapEmpty(Heap *minHeap)
+{
     return minHeap->size == 0;
 }
 
-bool insideOfMinHeap(Heap *minHeap, int value) {
-    if (minHeap->position[value] < minHeap->size)
+// verifica  se o vertice esta no heap
+bool insideOfMinHeap(Heap *minHeap, int value)
+{
+    if (minHeap->position[value] < minHeap->size) {
         return true;
+    }
+
     return false;
 }
 
-void decreaseKey(Heap *minHeap, int value, int key) {
-    int i = minHeap->position[value];
+// Diminui o custo da aresta de um vertice e atualiza as posições no heap
+void decreaseCost(Heap *minHeap, int value, int key)
+{
+    int index = minHeap->position[value];
 
-    minHeap->array[i]->key = key;
-
-    while (i && minHeap->array[i]->key < minHeap->array[(i - 1) / 2]->key)
+    minHeap->array[index]->key = key;
+    
+    while (index &&
+           minHeap->array[index]->key < minHeap->array[(index - 1) / 2]->key) //O(log n)
     {
-        minHeap->position[minHeap->array[i]->value] = (i - 1) / 2;
-        minHeap->position[minHeap->array[(i - 1) / 2]->value] = i;
-        swapHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+        minHeap->position[minHeap->array[index]->value] = (index - 1) / 2;
+        minHeap->position[minHeap->array[(index - 1) / 2]->value] = index;
+        swapHeapNode(&minHeap->array[index], &minHeap->array[(index - 1) / 2]);
 
-        i = (i - 1) / 2;
+        index = (index - 1) / 2; // move pro index do pai
     }
 }
 
-// file method
-bool isFileExists(FILE *file, char *filename ){
-    if (file == NULL) {
+// Metodos para manipulação de arquivos
+bool isFileExists(FILE *file, char *filename)
+{
+    if (file == NULL)
+    {
         printf("Could not open file %s", filename);
-        exit(1);
+        exit(1); // erro
     }
 }
 
